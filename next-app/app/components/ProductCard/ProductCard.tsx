ce9fd7ee7dcddf5a14199192f63a6aeaa0ee0642
Mona Kalu <ukpaimona@gmail.com>
2024-03-16 13:18:27 -0500

    Added taiwindcss

--- next-app/app/components/ProductCard/ProductCard.tsx
+++ next-app/app/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
-  return <div className={styles.card}>AddToCart</div>;
+  return (
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600">
+      AddToCart
+    </div>
+  );
 };
 
 export default ProductCard;
