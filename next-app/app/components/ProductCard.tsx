28d87beb72059b5198d2bbe87462d61323c4896b
Mona Kalu <ukpaimona@gmail.com>
2024-03-16 13:03:08 -0500

    CSS Styling

--- next-app/app/components/ProductCard.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProductCard = () => {
-  return <div>AddToCart</div>;
-};
-
-export default ProductCard;
---
+++ next-app/app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ProductCard.module.css";
+
+const ProductCard = () => {
+  return <div className={styles.card}>AddToCart</div>;
+};
+
+export default ProductCard;
