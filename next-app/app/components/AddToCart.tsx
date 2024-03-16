d71003e6aa985c87101e6d007ac81f279ad68f01
Mona Kalu <ukpaimona@gmail.com>
2024-03-16 10:34:42 -0500

    Updated ProductCard

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use clinet";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Click")}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
